ac555758b84b4d4cc4f68469a0f599a881f6b0da
MikeGray31 <michaelkgray31@gmail.com>
2019-11-15 11:20:50 -0500

    Finished

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,69 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        Integer[] removeThese = this.array;
+
+        for (Integer integer : this.array) {
+
+            if (getNumberOfOccurrences(integer) >= maxNumberOfDuplications) {
+                removeThese = removeValue(removeThese, integer);
+            }
+        }
+        return removeThese;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        Integer[] removeThese = this.array;
+
+        for (Integer integer : this.array) {
+
+            if (getNumberOfOccurrences(integer) == exactNumberOfDuplications) {
+                removeThese = removeValue(removeThese, integer);
+            }
+        }
+        return removeThese;
+    }
+
+
+    // gets the number of occurrences of a single object in the array
+    private Integer getNumberOfOccurrences(Integer objectToCount) {
+
+        int counter = 0;
+
+        for (Object s : this.array){
+
+            if (s.equals(objectToCount)){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    // removes all objects in the array that are equal to the given parameter
+    private Integer[] removeValue(Integer[] intArray,Integer objectToRemove) {
+        int counter = 0;
+        Integer[] keepthese = new Integer[intArray.length];
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] != objectToRemove) {
+                keepthese[i - counter] = intArray[i];
+            } else {
+                counter++;
+            }
+        }
+        Integer[] result = new Integer[intArray.length - counter];
+
+        for (int j = 0; j < result.length; j++) {
+            result[j] = keepthese[j];
+        }
+        return result;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,68 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        String[] removeThese = this.array;
+
+        for (String s : this.array) {
+
+            if (getNumberOfOccurrences(s) >= maxNumberOfDuplications) {
+                removeThese = removeValue(removeThese, s);
+            }
+        }
+        return removeThese;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        String[] removeThese = this.array;
+
+        for (String s : this.array) {
+
+            if (getNumberOfOccurrences(s) == exactNumberOfDuplications) {
+                removeThese = removeValue(removeThese, s);
+            }
+        }
+        return removeThese;
+    }
+
+
+    // gets the number of occurrences of a single object in the array
+    private Integer getNumberOfOccurrences(String objectToCount) {
+
+        int counter = 0;
+
+        for (Object s : this.array){
+
+            if (s.equals(objectToCount)){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    // removes all objects in the array that are equal to the given parameter
+    private String[] removeValue(String[] strArray,String objectToRemove) {
+        int counter = 0;
+        String[] keepthese = new String[strArray.length];
+        for (int i = 0; i < strArray.length; i++) {
+            if (strArray[i] != objectToRemove) {
+                keepthese[i - counter] = strArray[i];
+            } else {
+                counter++;
+            }
+        }
+        String[] result = new String[strArray.length - counter];
+
+        for (int j = 0; j < result.length; j++) {
+            result[j] = keepthese[j];
+        }
+        return result;
+    }
 }
